e5fdcd099442cd6cd7dc0dfbfa31e356c87f364f
Mohamed <mohammed.mmaljoke@gmail.com>
2023-01-31 14:21:48 -0300

    feat: auth by role middleware

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -12,6 +12,9 @@ export default class User extends BaseModel {
   @column()
   public email: string;
 
+  @column()
+  public roleId: string;
+
   @column({ serializeAs: null })
   public password: string;
 
--- start/routes/Subjects.routes.ts
+++ start/routes/Subjects.routes.ts
@@ -4,8 +4,13 @@ export default function subjectRoutes() {
   Route.group(() => {
     Route.get("/", "SubjectsController.fetchSubjects");
     Route.get("/:id", "SubjectsController.getSubject");
+  }).prefix("/subjects");
+
+  Route.group(() => {
     Route.post("/", "SubjectsController.create");
     Route.put("/:id", "SubjectsController.update");
     Route.delete("/:id", "SubjectsController.delete");
-  }).prefix("/subjects");
+  })
+    .prefix("/subjects")
+    .middleware(["auth", "role:admin"]);
 }
