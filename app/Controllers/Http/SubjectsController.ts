1b82c027d08cf7e812f117afb69f2996f391a172
Mohamed <mohammed.mmaljoke@gmail.com>
2023-01-31 11:29:26 -0300

    feat: fetch subjects

--- app/Controllers/Http/SubjectsController.ts
+++ app/Controllers/Http/SubjectsController.ts
@@ -3,17 +3,31 @@ import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Subject from "App/Models/Subject";
 import SubjectsServices from "App/Services/SubjectsServices";
 import SubjectValidator from "App/Validators/SubjectValidator";
-import { createdResponse } from "App/utils/http-response";
+import { createdResponse, serverErrorResponse } from "App/utils/http-response";
 
 //so that we can inject the service in the class
 @inject()
 export default class SubjectsController {
   constructor(private subjectsServices: SubjectsServices) {}
+  public async fetchSubjects({ response }: HttpContextContract) {
+    try {
+      const subjects = await this.subjectsServices.fetchSubjectsService();
+      return createdResponse<Subject[]>(response, subjects);
+    } catch (error) {
+      console.log("fetch subjects error", error);
+      return serverErrorResponse(response);
+    }
+  }
   public async create({ request, response }: HttpContextContract) {
     const { subjectName } = await request.validate(SubjectValidator);
-    const subject = await this.subjectsServices.createSubjectService(
-      subjectName
-    );
-    return createdResponse<Subject>(response, subject);
+    try {
+      const subject = await this.subjectsServices.createSubjectService(
+        subjectName
+      );
+      return createdResponse<Subject>(response, subject);
+    } catch (error) {
+      console.log("create subject error", error);
+      return serverErrorResponse(response);
+    }
   }
 }
--- app/Services/SubjectsServices.ts
+++ app/Services/SubjectsServices.ts
@@ -2,6 +2,9 @@ import Subject from "App/Models/Subject";
 
 export default class SubjectsServices {
   constructor() {}
+  public async fetchSubjectsService(): Promise<Subject[]> {
+    return await Subject.query();
+  }
   public async createSubjectService(subject: string): Promise<Subject> {
     return await Subject.create({ subjectName: subject });
   }
