ddb3caf60578466f8b4fbb6e680c5b34f6251aff
Mohamed <mohammed.mmaljoke@gmail.com>
2023-01-31 13:59:45 -0300

    feat: logout route

--- start/routes/Auth.routes.ts
+++ start/routes/Auth.routes.ts
@@ -4,5 +4,6 @@ export default function authRoutes() {
   Route.group(() => {
     Route.post("/register", "AuthController.register");
     Route.post("/login", "AuthController.login");
+    Route.post("/logout", "AuthController.logout");
   }).prefix("/auth");
 }
