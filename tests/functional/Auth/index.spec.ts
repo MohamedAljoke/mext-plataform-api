8d71bf740ac043e1ca4b3c830c02afdf1deef71d
Mohamed <mohammed.mmaljoke@gmail.com>
2023-02-05 13:46:17 -0300

    test: ensure user register with success

--- tests/functional/Auth/index.spec.ts
+++ tests/functional/Auth/index.spec.ts
@@ -9,6 +9,22 @@ test.group("Auth index", (group) => {
     await Database.beginGlobalTransaction();
     return () => Database.rollbackGlobalTransaction();
   });
+  test("Should register user and receives user data", async ({
+    client,
+    assert,
+  }) => {
+    const response = await client.post("/api/v1/auth/register").form({
+      ...registerUserData,
+      password_confirmation: registerUserData.password,
+    });
+    assert.properties(response.body(), [
+      "email",
+      "name",
+      "created_at",
+      "id",
+      "updated_at",
+    ]);
+  });
   test("Should login user successfully and receives token", async ({
     client,
     assert,
