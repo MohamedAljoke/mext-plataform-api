5ce3cc68cda392768909f68fa010006b83d13540
Mohamed <mohammed.mmaljoke@gmail.com>
2023-02-19 19:48:33 -0300

    feaT: add alternative model

--- app/Models/Alternative.ts
+++ app/Models/Alternative.ts
@@ -1,13 +1,26 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from "luxon";
+import { BaseModel, BelongsTo, belongsTo, column } from "@ioc:Adonis/Lucid/Orm";
+import Question from "./Question";
 
 export default class Alternative extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
+
+  @column()
+  public questionId: string;
+
+  @column()
+  public alternativeText: string;
+
+  @column()
+  public isCorrect: boolean;
+
+  @belongsTo(() => Question, { foreignKey: "questionId" })
+  public question: BelongsTo<typeof Question>;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 }
