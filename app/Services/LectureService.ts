d67528453a05f0d787c59456510d4fa5571b9d0d
Mohamed <mohammed.mmaljoke@gmail.com>
2023-02-20 14:11:56 -0300

    feat: get questions on get lecture

--- app/Services/LectureService.ts
+++ app/Services/LectureService.ts
@@ -23,7 +23,11 @@ export default class LecturesServices {
 
   public async getLectuerService(id: number) {
     const lectuer = await Lecture.find(id);
-    await Promise.all([lectuer?.load("video"), lectuer?.load("pdfs")]);
+    await Promise.all([
+      lectuer?.load("video"),
+      lectuer?.load("pdfs"),
+      lectuer?.load("questions"),
+    ]);
     return lectuer;
   }
 
