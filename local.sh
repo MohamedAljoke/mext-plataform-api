c5da7b3a9a41949837bba0a93d65457650343b9d
Mohamed <mohammed.mmaljoke@gmail.com>
2023-07-03 15:36:16 -0300

    fix get out

--- local.sh
+++ local.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
+# Start container
 docker-compose up -d
 
+# Check if MySQL is up
+start_time=$(date +%s)
 password="mextmysql"
 while true; do
     mysql -h "localhost" -u "mext" -p"$password" -e "quit" >/dev/null 2>&1
@@ -7,9 +12,17 @@ while true; do
         echo "MySQL connection successful!"
         break
     else
+        end_time=$(date +%s)
+        time_diff=$((end_time - start_time))
+        if [ $time_diff -gt 50 ]; then
+            break
+        fi
         echo "MySQL connection failed. Retrying in 5 seconds..."
         sleep 5
     fi
 done
+
+# Run migrations
 node ace migration:run
-echo done
+
+echo "Done."
